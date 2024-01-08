701ba70bf1e09ff091d9f2468b61292f0bbe0034
BHUVANESH REDDY <bhuvaneshreddy914@gmail.com>
2024-01-08 23:09:52 +0530

    My City Application

---
+++ app/src/main/java/com/example/cityinformation/Response/ContactsView.kt
@@ -0,0 +1,10 @@
+package com.example.cityinformation.Response
+
+import com.google.gson.annotations.SerializedName
+
+data class ContactsView (
+    @SerializedName("id"            ) var id           : String? = null,
+    @SerializedName("userid"        ) var userid       : String? = null,
+    @SerializedName("name"          ) var name         : String? = null,
+    @SerializedName("mobile_number" ) var mobileNumber : String? = null
+)
